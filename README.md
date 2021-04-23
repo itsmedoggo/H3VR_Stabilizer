38650b5bc4a480b8408b741f7e1c08be7291e1d7
itsmedoggo <38803896+itsmedoggo@users.noreply.github.com>
2021-04-23 15:33:31 -0500

    Update to Deli 0.3.x

---
+++ README.md
@@ -0,0 +1,4 @@
+# Stabilizer
+A simple [Deli](https://github.com/Deli-Collective/Deli) plugin that allows you to two-hand-stabilize while holding any object in your offhand. The vanilla game only allows two-hand-stabilizing while not holding anything, or holding a flashlight or magazine.
+
+Requires [BepInEx](https://github.com/BepInEx/BepInEx) 5.4.x and [Deli](https://github.com/Deli-Collective/Deli) 0.3.x. Place the Stabilizer.deli file into the H3VR/Deli/mods folder.
---
+++ Stabilizer/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Deli.Setup;
+using FistVR;
+using UnityEngine;
+
+namespace Stabilizer
+{
+    public class Plugin : DeliBehaviour
+    {
+        public readonly string GUID;
+        public readonly string STABILIZER_GUID;
+        public static Plugin Instance { get; private set; }
+
+        public Plugin()
+        {
+            GUID = Info.Guid;
+            STABILIZER_GUID = GUID + ".stabilizer";
+            Instance = this;
+            Harmony st = new Harmony(STABILIZER_GUID);
+            st.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(FVRFireArm), "IsTwoHandStabilized")]
+    internal class Patch
+    {
+        [HarmonyPrefix]
+        static bool IsTwoHandStabilized_Patch(FVRFireArm __instance, ref bool __result)
+        {
+            __result = false;
+            if (__instance.m_hand != null && __instance.m_hand.OtherHand != null)
+            {
+                float num = Vector3.Distance(__instance.m_hand.PalmTransform.position, __instance.m_hand.OtherHand.PalmTransform.position);
+                if (num < 0.15f)
+                {
+                    __result = true;
+                }
+            }
+            return false;
+        }
+    }
+}
